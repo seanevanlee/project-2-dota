358f1acc0b9ad2b4ec6f63f1d50a4a07dd1a9433
seanevanlee <seanevanlee@gmail.com>
2022-12-09 06:52:09 -0800

    Redirect is set up to go to DOTA official website

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ router.get(
 router.get("/logout", function (req, res) {
   req.logout(function () {
     //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
-    res.redirect("/heroes"); // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect("https://www.dota2.com/home"); // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   });
 });
 
